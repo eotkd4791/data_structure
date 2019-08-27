c1219d1c2c229c4dd6c9e4fb883d8fbb27d887e4
stef <eotkd4791@gmail.com>
2019-08-27 22:48:06 +0900

    [BOJ1918] 후위 표기식

--- Stack/BJ1918.cpp
+++ Stack/BJ1918.cpp
@@ -5,81 +5,78 @@
 #include <cstring>
 using namespace std;
 
-int main() {
-	stack <char> s;
-	char infix[101] = {}; 
-	char postfix[101] = {};
-	scanf("%s", infix);
+stack <char> s;
+char infix[101];
 
-	printf("1");
-	int idx = 0; bool open = 0;
+int main() {
+	scanf("%s", infix);
 
 	int infix_len = strlen(infix);
 	for (int i = 0; i < infix_len; i++) {
-		printf("2");
 		if (infix[i] == '+' || infix[i] == '-') {
-			if (!open) {
-				if (s.size() == 1) {
-					postfix[idx++] = s.top();
+			if (s.empty()) 
+				s.push(infix[i]);
+			else if (s.size() == 1) {
+				if (s.top() != '(') {
+					printf("%c", s.top());
 					s.pop();
-					s.push(infix[i]);
 				}
-				else if (s.size() > 1) {
-					while (!s.empty()) {
-						printf("3");
-						postfix[idx++] = s.top();
+				s.push(infix[i]);
+			}
+			else if (s.size() > 1) {
+				if (s.top() != '(') {
+					while (1) {
+						if (s.empty())
+							break;
+						else
+							if (s.top() == '(')
+								break;
+						printf("%c", s.top());
 						s.pop();
 					}
+				}
+				s.push(infix[i]);
+			}
+		}
+
+		else if (infix[i] == '/' || infix[i] == '*') {
+			if (s.size() >= 1) {
+				if (s.top() == '*' || s.top() == '/') {
+					printf("%c", s.top());
+					s.pop();
+					s.push(infix[i]);
 				}
 				else 
 					s.push(infix[i]);
 			}
-			else
-				s.push(infix[i]);
-		}
-		else if (infix[i] == '/' || infix[i] == '*') {
-			if (!open) {
-				if (s.size() == 1) {
-					if (s.top() == '+' || s.top() == '-') {
-						s.push(infix[i]);
-					}
-					else if (s.top() == '/' || s.top() == '*') {
-						postfix[idx++] = s.top();
-						s.push(infix[i]);
-					}
-				}
-				else if (s.size() > 1) {
-					while (!s.empty()) {
-						postfix[idx++] = s.top();
-						s.pop();
-					}
-				}
-				else
-					s.push(infix[i]);
-			}
 			else 
 				s.push(infix[i]);
-			
 		}
+
 		else if (infix[i] == '(' || infix[i] == ')') {
-			if (infix[i] == '(') {
-				open = 1;
+			if (infix[i] == '(') 
 				s.push(infix[i]);
-			}
 			else {
-				open = 0;
-				while (s.top() != '(') {
-					postfix[idx++] = s.top();
+				if (!s.empty()) {
+					while (s.top() != '(') {
+						printf("%c", s.top());
+						s.pop();
+					}
 					s.pop();
 				}
-				s.pop();
 			}
 		}
-		else 
-			postfix[idx++] = infix[i];
+		else
+			printf("%c", infix[i]);
 	}
-	int postfix_len = strlen(postfix);
-	for (int i = 0; i < postfix_len; i++) 
-		printf("%c", postfix[i]);
+	while (1) {
+		if (s.empty())
+			break;
+		else
+			if (s.top() == '(')
+				break;
+		printf("%c", s.top());
+		s.pop();
+	}
 	return 0;
 }
